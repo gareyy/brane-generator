d4733289fafccbd21da353805b01d81d5e581575
gareyyy <gk10guce@gmail.com>
2026-09-10 19:59:44 +1000

    CIFAR: download setup

---
+++ resnet18-cifar.py
@@ -0,0 +1,24 @@
+import torch
+import torchvision
+from torchvision.transforms import v2
+
+# from https://docs.pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+transform = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
